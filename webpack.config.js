91aae6f5874ec55974f570d4239604d9cbbe042a
rs-nganttt <nganttt@runsystem.net>
2024-08-28 16:51:26 +0700

    feat: update icon config

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,10 @@ module.exports = {
   },
   plugins: [
     new CopyPlugin({
-      patterns: [{ from: "manifest.json", to: "../manifest.json" }],
+      patterns: [
+        { from: "manifest.json", to: "../manifest.json" },
+        { from: "icons", to: "../icons" },
+      ],
     }),
     ...getHtmlPlugins(["index"]),
   ],
